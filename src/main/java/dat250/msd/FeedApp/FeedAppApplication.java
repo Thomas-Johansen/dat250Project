278e68913ce7a2af28db55d3736a0291ad9d32e5
Vegard Dæmring <vegarddem@gmail.com>
2023-09-11 11:25:00 +0200

    init

---
+++ src/main/java/dat250/msd/FeedApp/FeedAppApplication.java
@@ -0,0 +1,13 @@
+package dat250.msd.FeedApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dat250/msd/FeedApp/FeedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dat250.msd.FeedApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
