d21205ce4aebe3b8b8ca9e6298ad5bb13774d1ab
Vegard Demring <vegarddem@gmail.com>
2023-10-06 22:30:03 +0200

    Switched from gson to fasterxml to fix LocalDateTime serialization error in tests.

--- src/test/java/dat250/msd/FeedApp/UserControllerTest.java
+++ src/test/java/dat250/msd/FeedApp/UserControllerTest.java
@@ -1,16 +1,13 @@
 package dat250.msd.FeedApp;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dat250.msd.FeedApp.model.UserData;
 import okhttp3.*;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.List;
 import java.util.Objects;
 
-import org.apache.catalina.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,9 +15,6 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
@@ -30,13 +24,13 @@ public class UserControllerTest {
     private int port;
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private final OkHttpClient client = new OkHttpClient();
-    private final Gson gson = new Gson();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     private String getBaseURL() {
         return "http://localhost:" + port + "/";
     }
-    private String doPostRequest(UserData user){
-        RequestBody body = RequestBody.create(gson.toJson(user), JSON);
+    private String doPostRequest(UserData user) throws JsonProcessingException {
+        RequestBody body = RequestBody.create(objectMapper.writeValueAsString(user), JSON);
 
         Request request = new Request.Builder()
                 .url(getBaseURL() + "user")
@@ -74,24 +68,24 @@ public class UserControllerTest {
     }
 
     @Test
-    void testCreateUser() {
+    void testCreateUser() throws JsonProcessingException {
         final UserData user = new UserData();
         user.setUsername("Truls");
         user.setPassword("Password123");
         user.setEmail("TrulsCool@gmail.com");
-        final UserData createdUser = gson.fromJson(doPostRequest(user), UserData.class);
+        final UserData createdUser = objectMapper.readValue(doPostRequest(user), UserData.class);
         System.out.println("The user_id is "+ createdUser.getUsername());
         assertNotNull(createdUser);
     }
     @Test
-    void testGetUser(){
+    void testGetUser() throws JsonProcessingException {
         final UserData user = new UserData();
         user.setUsername("Karl");
         user.setPassword("glhf");
         user.setEmail("KarlMarks@gmail.com");
 
-        final UserData createdUser = gson.fromJson(doPostRequest(user), UserData.class);
-        final UserData returnedUser = gson.fromJson(doGetRequest(createdUser.getUsername(),createdUser.getPassword()), UserData.class);
+        final UserData createdUser = objectMapper.readValue(doPostRequest(user), UserData.class);
+        final UserData returnedUser = objectMapper.readValue(doGetRequest(createdUser.getUsername(),createdUser.getPassword()), UserData.class);
         System.out.println("The user_id is "+ returnedUser.getUsername());
 
         assertEquals(createdUser.getUsername(),returnedUser.getUsername());
