b75fb7e88c4953da3a9555058e4db68ff7ee8fcb
Mikal <mdr015@uib.no>
2023-11-29 15:41:43 +0100

    Did some changes to Cors Config

--- src/main/java/dat250/msd/FeedApp/configuration/WebConfig.java
+++ src/main/java/dat250/msd/FeedApp/configuration/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://host.docker.internal:80")
                         .allowedMethods(
                                 HttpMethod.GET.name(),
                                 HttpMethod.POST.name(),
